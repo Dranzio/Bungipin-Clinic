bb9bd449793a8710080154dd92037a454268448f
ecaquino <ecaquino@mymail.mapua.edu.ph>
2026-09-19 19:58:17 +0800

    Basic SQL querying for patientRecords.html

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const mysqlPool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Sep2024101585*!',
+    password: 'password',
     database: 'dental_appointments',
     waitForConnections: true,
     connectionLimit: 10,
