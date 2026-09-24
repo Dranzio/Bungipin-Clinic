e92e08b3e1ed6483cda9b0ebbdaa6953816081d3
ecaquino <ecaquino@mymail.mapua.edu.ph>
2026-09-24 12:49:57 +0800

    working version

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const registerPatientRecordsRoutes = require("./Employee/PatientRecords");
 const registerDashboardRoutes = require("./Admin/DashboardRoutes");
 const registerUserManagementRoutes = require("./Admin/UserManage");
 const registerServiceRoutes = require("./Admin/ServiceRoutes");
+const registerHistoryRoutes = require("./Customer/HistoryRoutes");
 
 const app = express();
 app.use(express.json({ limit: "5mb" })); // service icons arrive as base64
@@ -28,6 +29,7 @@ registerPatientRecordsRoutes(app, db);
 registerDashboardRoutes(app, db);
 registerUserManagementRoutes(app, db);
 registerServiceRoutes(app, db);
+registerHistoryRoutes(app, db);
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname)));
--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const mysqlPool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Sep2024101585*!',
     database: 'dental_appointments',
     waitForConnections: true,
     connectionLimit: 10,
